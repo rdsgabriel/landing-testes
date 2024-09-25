54ebc96bb1609db7b53957cfe6269ce8d883e6b9
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2024-09-25 12:25:33 -0300

    ports

--- app/api/reverseproxy/route.ts
+++ app/api/reverseproxy/route.ts
@@ -2,21 +2,32 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const port = req.query.port; // Espera que a porta venha na query
+
+  let targetUrl = '';
+  if (port === '9090') {
+    targetUrl = 'http://35.199.77.49:9090/api/v1/auth/login';
+  } else if (port === '8081') {
+    targetUrl = 'http://35.199.77.49:8081/api/v1/auth/login'; // Ajuste a URL conforme necessário
+  } else {
+    return res.status(400).json({ message: 'Porta não suportada.' });
+  }
+
   if (req.method === 'POST') {
     try {
-      const response = await fetch('http://35.199.77.49:9090/api/v1/auth/login', {
+      const response = await fetch(targetUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(req.body), // Passa o corpo da requisição
+        body: JSON.stringify(req.body),
       });
 
-      const text = await response.text(); // Capture a resposta como texto
+      const text = await response.text();
 
       let data;
       try {
-        data = JSON.parse(text); // Tente analisar o texto como JSON
+        data = JSON.parse(text);
       } catch (error) {
         console.error('Error parsing JSON:', error);
         return res.status(500).json({ message: 'Erro ao processar a resposta da API.' });
