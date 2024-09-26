c203af9899e7f476f4bfa9e8107a5c5725ef7b9d
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2024-09-26 20:48:09 -0300

    teste

--- app/login/login-tab.tsx
+++ app/login/login-tab.tsx
@@ -11,8 +11,9 @@ import { useRouter } from 'next/navigation';
 import Cookies from 'js-cookie';
 
 
-const GOOGLE_AUTH_URL = process.env.NEXT_PUBLIC_GOOGLE_AUTH_URL ?? '';
+const GOOGLE_AUTH_URL = 'https://api-taskfreela.zapto.org/oauth2/authorize/google?redirect_uri=https://www.taskfreela.com/api/oauth2/google/redirect'
 const GITHUB_AUTH_URL = process.env.NEXT_PUBLIC_GITHUB_AUTH_URL ?? '';
+console.log(GOOGLE_AUTH_URL)
 
 
 interface FormData {
