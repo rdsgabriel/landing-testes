9769c6183442cb65aa7e7173bf849b0f059432c6
rdsgabriel <gabriel.rodrigues.ismart@gmail.com>
2024-09-27 16:35:18 -0300

    redirect to workspace

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,20 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  
   const token = req.cookies.get('token');
   
+  // Se o usuário tentar acessar a rota /login e já tiver um token, redireciona para /workspace
+  if (req.nextUrl.pathname === '/login' && token) {
+    return NextResponse.redirect(new URL('/workspace', req.url));
+  }
 
-  if (!token) {
+  if (!token && req.nextUrl.pathname.startsWith('/workspace')) {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
+
   return NextResponse.next();
 }
-
 export const config = {
-  matcher: ['/workspace/:path*'],
+  matcher: ['/workspace/:path*', '/login'],
 };
